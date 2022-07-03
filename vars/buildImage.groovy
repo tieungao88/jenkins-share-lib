b585f8eb0b4302beadbcc5c4e9dbd2cb437b734c
Bui Manh Tien <tienbm@Buis-MacBook-Air.local>
2022-07-03 09:16:16 +0700

    update

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
-def call(String imagename) {
+def call(String imagename, String imagetags) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t tieungao1988/${imagename} ."
+        sh "docker build -t tieungao1988/${imagename}:${imagetags} ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push tieungao1988/${imagename}"
+        sh "docker push tieungao1988/${imagename}:${imagetags}"
     }
 }
